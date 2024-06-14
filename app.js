02a872d2f32dfffc9df8dce22c99701b893241ad
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-06-14 22:22:50 +0530

    Create an API to add user in to the db

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-const express = require("express");
-const server = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const createUser = require('./src/models/User'); // Import the User model
 
-server.use((req,res,next)=>{
-    console.log(`Hit for ${req.url}`);
-    next();
-})
+const app = express();
+const port = 5000;
 
-server.get("/get-data", (req,res)=>{
-    res.write("Hello ");
-    res.end("[1,2,3,4,5,6,7]")
-})
+// Middleware to parse JSON
+app.use(express.json());
 
-server.listen(5000, ()=>{
-    console.log("Started service on Port 5000");
-})
+// Connect to MongoDB
+const mongoURI = 'mongodb://127.0.0.1:27017/sample-db'; // Replace with your MongoDB URI
+mongoose.connect(mongoURI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Route to create a new user
+app.post('/users', async (req, res) => {
+  try {
+    const user = new createUser(req.body);
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Route to get all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
