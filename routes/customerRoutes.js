1a02a3cb0ff6214e6f3360e097758eaf6a6e3390
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-06-24 22:29:18 +0530

    Added fetch restaurants api

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -4,5 +4,6 @@ const customerController = require("../controllers/customerController");
 
 router.post("/register", customerController.register);
 router.post("/login", customerController.login);
+router.get("/fetchRestaurants/:pincode", customerController.fetchRestaurants);
 
 module.exports = router;
