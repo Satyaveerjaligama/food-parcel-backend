3d4421bb6739cd94db98ddd534a2cceffae250c4
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-06-25 21:44:18 +0530

    Moved restaurants related apis

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -36,3 +36,25 @@ exports.login = async (req,res) => {
         res.status(400).json({message: err.message})
     })
 }
+
+// This API will fetch all restaurants based on the pincode that we provide
+exports.fetchRestaurants = async(req, res) => {
+    const pincode = req.params?.pincode;
+    try {
+        const restaurants = await Restaurant.find({pincode},{restaurantId: 1, restaurantName: 1, restaurantType: 1, _id: 0});
+        res.status(200).json(restaurants);
+    } catch(err) {
+        res.status(400).json({message: err.message})
+    }
+};
+
+// This api will fetch the restaurant details based on the restaurant id
+exports.fetchRestaurantDetails = async(req, res) => {
+    const restaurantId = req.params?.restaurantId;
+    try {
+        const restaurants = await Restaurant.find({restaurantId},{restaurantId: 1, restaurantName: 1, restaurantType: 1, _id: 0});
+        res.status(200).json(restaurants[0]);
+    } catch(err) {
+        res.status(400).json({message: err.message})
+    }
+}
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -4,5 +4,7 @@ const restaurantControllers = require("../controllers/restaurantController");
 
 router.post("/register", restaurantControllers.register);
 router.post("/login", restaurantControllers.login);
+router.get("/fetchRestaurants/:pincode", restaurantControllers.fetchRestaurants);
+router.get("/fetchRestaurantDetails/:restaurantId", restaurantControllers.fetchRestaurantDetails);
 
 module.exports = router;
