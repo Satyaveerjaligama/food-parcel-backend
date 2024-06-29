e8c451ed6a99e8421c2f1efa7711bd0956d67be2
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-06-29 22:35:21 +0530

    Added image in fetchRestaurants api res

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const restaurantControllers = require("../controllers/restaurantController");
+const { attachGFS } = require("../config/gridfs");
 
 router.post("/register", restaurantControllers.register);
 router.post("/login", restaurantControllers.login);
-router.get("/fetchRestaurants/:pincode", restaurantControllers.fetchRestaurants);
+router.get("/fetchRestaurants/:pincode", attachGFS, restaurantControllers.fetchRestaurants);
 router.get("/fetchRestaurantDetails/:restaurantId", restaurantControllers.fetchRestaurantDetails);
 
 module.exports = router;
